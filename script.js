5cc5e19155bc18ee32596e4a9870052089b72da0
Charnice Tatum <charnicetatum@gmail.com>
2021-12-31 09:57:40 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// let dog = {
+//     name: "Steve",
+//     age: 1,
+//     location: "Japan",
+//     profileImg: "https://images.unsplash.com/photo-1613410180211-106788bd2d94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80",
+//     bannerImg: "url('https://thumbs.dreamstime.com/b/green-polka-dot-background-25383307.jpg')",
+//     backgroundColor: "#00FFFF",
+//     stats: {
+//         followers: "1.2M",
+//         likes: "8M",
+//         posts: "2K"
+//     }
+// }
+
+// document.getElementById("fullName").innerText = dog.name;
+// document.getElementById('fullName').style.color = 'green'
+// document.getElementById("age").innerText = dog.age;
+// document.getElementById("location").innerText = dog.location;
+// document.getElementsByClassName('profileImg')[0].src = dog.profileImg;
+// document.getElementsByClassName('banner')[0].style.backgroundImage = dog.bannerImg;
+// document.getElementsByClassName('middle').style
+
+
+// let stats = document.getElementsByClassName('stats')
+
+// for(let i = 0; i < stats.length; i++) {
+//     console.log(stats[i].innerText)
+
+//     if(i == 0) {
+//         stats[i].innerText = dog.stats.followers
+//     }
+//     else if(i == 1) {
+//         stats[i].innerText = dog.stats.likes
+//     }
+//     else if(i == 2) {
+//         stats[i].innerText = dog.stats.posts
+//     }
+// }
+
+// document.body.style.backgroundColor = dog.backgroundColor
+
+// document.getElementsById('fullName').addEventListener('click', function(){
+//     document.getElementById('fullName').style.color = 'green'
+// })
